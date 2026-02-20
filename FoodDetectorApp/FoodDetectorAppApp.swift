5c0d9906b12a630e650de86f0961c26a9be09376
Bhagyashree Khatri <bhagyash.23khatri@gmail.com>
2026-02-20 15:28:32 +0800

    Initial Commit

---
+++ FoodDetectorApp/FoodDetectorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDetectorAppApp.swift
+//  FoodDetectorApp
+//
+//  Created by Kishor Pahalwani on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodDetectorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
